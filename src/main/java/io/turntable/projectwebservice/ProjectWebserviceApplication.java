c666084d9c523bccc73bd63ece46df4d55a9eca6
pkErbynn <john.erbynn@gmail.com>
2019-12-01 22:55:25 +0000

    search by name spill off

--- src/main/java/io/turntable/projectwebservice/ProjectWebserviceApplication.java
+++ src/main/java/io/turntable/projectwebservice/ProjectWebserviceApplication.java
@@ -29,6 +29,7 @@ import java.util.stream.Collectors;
 public class ProjectWebserviceApplication {
 
     private Object lock = new Object();
+    private int idSearchUserInput;
 //	public static void main(String[] args) {
 //		SpringApplication.run(ProjectWebserviceApplication.class, args);
 //		Sub.receiveMessage();
@@ -56,7 +57,7 @@ public class ProjectWebserviceApplication {
         /////////////////////////////////
         System.out.println("...inside cli");
         Scanner sn = new Scanner(System.in);
-        String userInput;
+        String mainMenuUserInput;
 
         while (true) {
             System.out.println("*************************************************************");
@@ -66,14 +67,15 @@ public class ProjectWebserviceApplication {
             System.out.println("##\t 1. View all projects");
             System.out.println("##\t 2. Search project");
             System.out.println("##\t 3. Add new project");
-            System.out.println("##\t 4. Delete project");
-            System.out.println("##\t 5. Quit this Application");
+            System.out.println("##\t 4. Update existing project");
+            System.out.println("##\t 5. Delete project");
+            System.out.println("##\t 6. Quit this Application");
             System.out.println("####################################################");
 
             System.out.println("Select option:");
-            userInput = sn.nextLine();
+            mainMenuUserInput = sn.nextLine();
 
-            switch (userInput) {
+            switch (mainMenuUserInput) {
                 case "1":
                     Thread getProjectThread = new Thread(() -> {
                         List<Project> projects = projectService.getAllProjects();
@@ -85,7 +87,11 @@ public class ProjectWebserviceApplication {
 //                        System.out.println(ids);
 
                         for (int i = 0; i < ids.size(); i++) {
-                            try {Thread.sleep(1000);} catch (InterruptedException e) {e.printStackTrace();}
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
                             System.out.println("*****************************************");
                             System.out.println(AnsiConsole.WHITE_BOLD + "\tProject id:   " + AnsiConsole.RESET + AnsiConsole.YELLOW + ids.get(i) + AnsiConsole.RESET);
                             System.out.println(AnsiConsole.WHITE_BOLD + "\tProject name: " + AnsiConsole.RESET + AnsiConsole.GREEN + names.get(i) + AnsiConsole.RESET);
@@ -95,56 +101,76 @@ public class ProjectWebserviceApplication {
                     }, "retrieveProjectThread");
 
                     getProjectThread.start();
-                    try {getProjectThread.join();} catch (InterruptedException e) {e.printStackTrace();}
+                    try {
+                        getProjectThread.join();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                     break;
 
-                case  "2":
+                case "2":
                     System.out.println("*********** Search Option *********");
                     System.out.println("1. Search by name");
                     System.out.println("2. Search by id");
+                    System.out.println("\nKindly enter preferred search option: ");
+//                    Scanner searchUserInput = new Scanner(System.in);
+                    int searchChoiceUserInput = sn.nextInt();
 
-                    int searchChoice = sn.nextInt();
-                    if(searchChoice == 1){
-                        System.out.println("Enter project name to search for :) ");
-                        userInput = sn.nextLine().toLowerCase();
-                        List<Project> projectsByNameSearch = projectService.getProjectByName(userInput);
-                        if (projectsByNameSearch.isEmpty()) {
-                            System.out.printf("Sorry...Invalid project name: %s", userInput);
-                        }
-                        else {
-                            List<Integer> ids = projectsByNameSearch.stream().map(e -> e.getProject_id()).collect(Collectors.toList());
-                            List<String> names = projectsByNameSearch.stream().map(e -> e.getProject_name()).collect(Collectors.toList());
-                            List<String> descs = projectsByNameSearch.stream().map(e -> e.getDescription()).collect(Collectors.toList());
+                    switch (searchChoiceUserInput) {
+                        case 1:
+                            System.out.println("Enter project name to search for :) ");
+                            String projectNameUserInput = sn.nextLine();        // hilarious
+                            System.out.println("....ip1");
+                            String searchByProjectNameUserInput = sn.nextLine();
+                            System.out.println("....ip2");
 
-                            for (int i = 0; i < ids.size(); i++) {
-                                try {Thread.sleep(1000);} catch (InterruptedException e) {e.printStackTrace();}
-                                System.out.println("*****************************************");
-                                System.out.println(AnsiConsole.WHITE_BOLD + "\tProject id:   " + AnsiConsole.RESET + AnsiConsole.YELLOW + ids.get(i) + AnsiConsole.RESET);
-                                System.out.println(AnsiConsole.WHITE_BOLD + "\tProject name: " + AnsiConsole.RESET + AnsiConsole.GREEN + names.get(i) + AnsiConsole.RESET);
-                                System.out.println(AnsiConsole.WHITE_BOLD + "\tProject desc: " + AnsiConsole.RESET + AnsiConsole.BLUE + descs.get(i) + AnsiConsole.RESET);
-                                System.out.println("*****************************************");
+                            List<Project> projectsByNameSearch = projectService.getProjectByName(searchByProjectNameUserInput);
+                            if (projectsByNameSearch.isEmpty()) {
+                                System.out.printf("Sorry...No record found: %s \n", searchByProjectNameUserInput);
+                            } else {
+                                List<Integer> ids = projectsByNameSearch.stream().map(e -> e.getProject_id()).collect(Collectors.toList());
+                                List<String> names = projectsByNameSearch.stream().map(e -> e.getProject_name()).collect(Collectors.toList());
+                                List<String> descs = projectsByNameSearch.stream().map(e -> e.getDescription()).collect(Collectors.toList());
+
+                                for (int i = 0; i < ids.size(); i++) {
+                                    try {
+                                        Thread.sleep(1000);
+                                    } catch (InterruptedException e) {
+                                        e.printStackTrace();
+                                    }
+                                    System.out.println("*****************************************");
+                                    System.out.println(AnsiConsole.WHITE_BOLD + "\tProject id:   " + AnsiConsole.RESET + AnsiConsole.YELLOW + ids.get(i) + AnsiConsole.RESET);
+                                    System.out.println(AnsiConsole.WHITE_BOLD + "\tProject name: " + AnsiConsole.RESET + AnsiConsole.GREEN + names.get(i) + AnsiConsole.RESET);
+                                    System.out.println(AnsiConsole.WHITE_BOLD + "\tProject desc: " + AnsiConsole.RESET + AnsiConsole.BLUE + descs.get(i) + AnsiConsole.RESET);
+                                    System.out.println("*****************************************");
+                                }
                             }
-                        }
+                            break;
 
-                    } else if (searchChoice == 2) {
-                        System.out.println("Enter project id to search for ");
-                        int idSearch = sn.nextInt();
-                        Project projectSearchById = projectService.getProjectById(idSearch);
+                        case 2:
+                            System.out.println("Enter project id to search for ");
+                            int idSearchUserInput = sn.nextInt();
+//                            if(idSearchUserInput){}
+                            Project projectSearchById = projectService.getProjectById(idSearchUserInput);
 
-                        if (projectSearchById == null) {
-                            System.out.printf("Sorry...Invalid project name: %s", userInput);
-                        }
-                        else {
+                            if (projectSearchById == null) {
+                                System.out.printf("Sorry...Invalid project name: %s", mainMenuUserInput);
+                            } else {
 //                                try {Thread.sleep(1000);} catch (InterruptedException e) {e.printStackTrace();}
                                 System.out.println("*****************************************");
                                 System.out.println(AnsiConsole.WHITE_BOLD + "\tProject id:   " + AnsiConsole.RESET + AnsiConsole.YELLOW + projectSearchById.getProject_id() + AnsiConsole.RESET);
-                                System.out.println(AnsiConsole.WHITE_BOLD + "\tProject name: " + AnsiConsole.RESET + AnsiConsole.GREEN + projectSearchById.getProject_name()+ AnsiConsole.RESET);
-                                System.out.println(AnsiConsole.WHITE_BOLD + "\tProject desc: " + AnsiConsole.RESET + AnsiConsole.BLUE + projectSearchById.setDescription() + AnsiConsole.RESET);
-                                System.out.println("*****************************************");
+                                System.out.println(AnsiConsole.WHITE_BOLD + "\tProject name: " + AnsiConsole.RESET + AnsiConsole.GREEN + projectSearchById.getProject_name() + AnsiConsole.RESET);
+                                System.out.println(AnsiConsole.WHITE_BOLD + "\tProject desc: " + AnsiConsole.RESET + AnsiConsole.BLUE + projectSearchById.getDescription() + AnsiConsole.RESET);
+                                System.out.println("*****************************************\n\n");
                             }
-                    }
-                        break;
+                            break;
+                        default:
+                            System.out.println("Invalid option: " + searchChoiceUserInput);
+                            break;
 
+                    }
+
+                    break;
                 case "3":
                     System.out.println("Enter project name? ");
                     String projectName = sn.nextLine().toLowerCase();
@@ -164,7 +190,7 @@ public class ProjectWebserviceApplication {
                     System.out.println("Exiting...");
                     System.exit(0);
                 default:
-                    System.out.println("Invalid option: " + userInput);
+                    System.out.println("Invalid option: " + mainMenuUserInput);
 
             }
         }
